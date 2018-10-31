90bc9d132e97998e3d737b90e2177d6c42c6b274
myapplestory <csun@ualberta.ca>
2018-10-31 16:46:55 -0600

    quik sav

--- ass4/selenium_test.py
+++ ass4/selenium_test.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
 
 
+
+
 # put your own path to chromedriver
 chrome_driver_binary = r"/mnt/c/Users/James/Downloads/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_binary)
 driver.get("http://162.246.157.231:8000")
 
-elem = driver.find_element_by_id("name")
-print(elem)
 
 
+
+def test_home():
+    nameElem    = driver.find_element_by_id("name")
+    aboutElem   = driver.find_element_by_id("about")
+    skillsElem  = driver.find_element_by_id("skills")
+    eduElem     = driver.find_element_by_id("education")
+    workXPElem  = driver.find_element_by_id("work")
+    contactElem = driver.find_element_by_id("contact")
+
+    assert nameElem != None
+    assert aboutElem != None
+    assert skillsElem != None
+    assert eduElem != None
+    assert workXPElem != None
+    assert contactElem != None
+
+    
+test_home()
